ab49837034adf84af151264c57a143dfc625f6ca
Zack Bostock <zbostock56@gmail.com>
2023-11-10 14:35:11 -0500

    Adjusted for pole crowding

--- include/sphere.h
+++ include/sphere.h
@@ -2,7 +2,8 @@
 #include <math.h>
 #include <sphere_str.h>
 
-#define NUM_POINTS (10000)
+int NUM_POINTS = 10000;
+float EPSILON = 0.5;
 
 /* ------------- INTERNALLY DEFINED FUNCITONS -----------*/
 void square_lattice();
--- src/sphere.c
+++ src/sphere.c
@@ -4,13 +4,13 @@ void square_lattice() {
   /* Golden ratio */
   float phi = (1 + sqrt(5)) / 2; 
   LATTICE_POINT points[NUM_POINTS];
-  for (int i = 0; i < NUM_POINTS; i++) {
-    float x = ((float) i) / phi;
+  for (float i = 0; i < (float) NUM_POINTS; i += 1.0) {
+    float x = i / phi;
     /* Keep just the decimal part of x */
     float x_int = (int) x;
     x -= (float) x_int;
-    points[i].x = x;
-    points[i].y = (float) (i / ((float) (NUM_POINTS - 1)));
+    points[(int) i].x = x;
+    points[(int) i].y = (i + EPSILON) / ((float) (NUM_POINTS - 1) + (2.0 * EPSILON));
   }
   for (int i = 0; i < NUM_POINTS; i++) {
     printf("%d: (%f, %f)\n", i, points[i].x, points[i].y);
